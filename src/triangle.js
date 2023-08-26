823e4eb92f855e512408f5f1e16d09f209372d13
Kristoffer Dyrkorn <>
2023-08-26 19:04:28 +0200

    Fixups

--- src/triangle.js
+++ src/triangle.js
@@ -20,8 +20,8 @@ export default class Triangle {
     this.lightDirection = new Vector(-1, -1, -1);
     this.lightDirection.normalize();
 
-    // w = edge distances - that are also converted to interpolation weights
-    this.w = new Vector();
+    // k = edge distances - that are also converted to interpolation weights
+    this.k = new Vector();
 
     // keep the normal vector in a separate variable.
     // assume flat shading, ie the normal is constant for the entire triangle.
@@ -32,7 +32,7 @@ export default class Triangle {
     this.p = new Vector();
 
     // distance functions (interpolation weights) along the left edge of the bounding rectangle
-    this.wLeft = new Vector();
+    this.kLeft = new Vector();
   }
 
   getDeterminant(a, b, c) {
@@ -63,7 +63,7 @@ export default class Triangle {
 
     Triangle.trianglesDrawn++;
 
-    // pre-store 1/determinant, to later replace divides by multiplication
+    // pre-store 1/determinant, to later replace divides by multiplications
     const invDeterminant = 1 / determinant;
 
     // create bounding box around triangle, expanding coordinates out to integer values
@@ -87,93 +87,85 @@ export default class Triangle {
     this.p[0] = xmin + 0.5;
     this.p[1] = ymin + 0.5;
 
-    // set up distance functions (interpolation weights) along the left edge of the bounding rectangle.
-    // values are kept un-normalized (ie not divided by determinant of full triangle) to retain precision.
-    // so these values are barycentric coordinates (0..1), multiplied by the determinant of the full triangle.
-    // wLeft[0] = distance from a to bc, wLeft[1] = from b to ca, wLeft[2] = from c to ab
-    this.wLeft[0] = this.getDeterminant(vb, vc, this.p);
-    this.wLeft[1] = this.getDeterminant(vc, va, this.p);
-    this.wLeft[2] = this.getDeterminant(va, vb, this.p);
+    // set up distance functions (interpolation weights) to use when traversing along the left edge of the
+    // bounding rectangle - which we do when filling the triangle.
+    // values here are kept un-normalized (ie not divided by the determinant of full triangle) to retain precision.
+    // ie, these values are barycentric coordinates (0..1), multiplied by the determinant of the full triangle.
+    // kLeft[0] = distance from a to bc, kLeft[1] = from b to ca, kLeft[2] = from c to ab
+    this.kLeft[0] = this.getDeterminant(vb, vc, this.p);
+    this.kLeft[1] = this.getDeterminant(vc, va, this.p);
+    this.kLeft[2] = this.getDeterminant(va, vb, this.p);
 
     // calculate per pixel / per line deltas for incremental evaluation of distance function
-    const dwdx = new Vector(vb[1] - vc[1], vc[1] - va[1], va[1] - vb[1]);
-    const dwdy = new Vector(vb[0] - vc[0], vc[0] - va[0], va[0] - vb[0]);
+    const dkdx = new Vector(vb[1] - vc[1], vc[1] - va[1], va[1] - vb[1]);
+    const dkdy = new Vector(vb[0] - vc[0], vc[0] - va[0], va[0] - vb[0]);
 
-    //    this.wLeft.quantize(4);
-    //    dwdx.quantize(4);
-    //    dwdy.quantize(4);
+    // the w coordinate contains the distance (parallel to z axis / view direction) from
+    // the camera to the vertex. (The clip and screen space transforms don't change it.)
 
-    // trick: after perspective transform, the z distance from camera to vertex
-    // is stored in the w coordinate. (The clip space and screen space transforms don't change it.)
-    // we reverse the sign to convert from a distance to a coordinate (as the camera is placed in origo)
+    // compute 1/w for each vertex, to later 1) do linear interpolation on the 1/w values,
+    // and 2) take the inverse at a given pixel - to get the "actual value" there when needed.
+    // this is the way to get perspective correct *attribute* interpolation.
+    const wInverted = new Vector(1 / va[3], 1 / vb[3], 1 / vc[3]);
 
-    // compute inverse z coordinates for each vertex, and do linear interpolation on the values,
-    // and then compute inverse again (ie get the "actual value") when it is needed.
-    // this way we get perspective correct z interpolation - since 1/z is linear.
-    // perspective correct z values are needed for correct texture mapping.
-    const zInverted = new Vector(1 / -va[3], 1 / -vb[3], 1 / -vc[3]);
+    // pre-multiply the inverted w values at the vertices by 1 / determinant
+    // so that this normalizing multiply is not needed after interpolating attribute values
+    // since the interpolation weights k are already scaled with determinant value in the inner loop, this
+    // premultiplication will compensate for it when the w's are multiplied by the weights
+    wInverted.scale(invDeterminant);
 
-    // pre-multiply inverted z values at the vertices by 1 / determinant
-    // so that this multiply is not needed when calculating each interpolated 1/z value
-    // (ie when we are dotting inverted z values from vertices with the weights, which are *scaled* barycentric coordinates)
-    // thus we save one multiply per pixel
-    zInverted.scale(invDeterminant);
-
-    // use projected z values for each vertex for the z buffer calculations.
-    // the projection matrix setup defines the z range, and the matrix values chosen there
-    // maximise the numerical precision. the range of the projected z value is
+    // use projected (perspective transformed) z values for each vertex for the z buffer calculations.
+    // using linear interpolation of already-perspective-transformed z values is the correct way to get
+    // *values to put in the z buffer*.
+    // the projection matrix will define the z range, and the spesific matrix values chosen there now
+    // maximise the numerical precision. the range of the projected z values are
     // 1 (near plane) to 0 (far plane, at infinity)
     // see https://developer.nvidia.com/content/depth-precision-visualized
     const projectedZ = new Vector(va[2], vb[2], vc[2]);
 
-    // use projected z for z buffer calculations.
-    // in the fill loop we use scaled barycentric weights (pre-multipled by determinant value)
-    // so pre-multiply here by the inverse - so the resulting values are correct
-    // TODO consider other ways of calculating z buffer values - skipping/shifting pre-multiplication,
-    // or adding constants dxdz and dydz per pixel
-    // TODO consider removing the scaling here, it should not be needed for the z buffer operation
-    // and might reduce precision
+    // in the fill loop we use scaled barycentric weights (pre-multipled by the determinant value)
+    // so pre-multiply here by 1 / determinant - so the resulting weighted values become correct
     projectedZ.scale(invDeterminant);
 
-    // texture x- and y-coordinates for each vertex, scaled by the corresponding inverse z value
-    // and by texture size. the last scaling is to convert from uv range (0..1) to raster coordinate range
-    const sDivZ = new Vector(
+    // texture x- and y-coordinates for each vertex, scaled by the corresponding inverse w value
+    // and by texture size. the last scaling is there to convert from uv range (0..1) to raster coordinate range
+    const sDivW = new Vector(
       textureCoordinates[this.ta][0],
       textureCoordinates[this.tb][0],
       textureCoordinates[this.tc][0]
     );
-    sDivZ.multiply(zInverted);
-    sDivZ.scale(255); // texture size of 256 pixels in x direction
+    sDivW.multiply(wInverted);
+    sDivW.scale(255); // texture size of 256 pixels in x direction
 
-    const tDivZ = new Vector(
+    const tDivW = new Vector(
       textureCoordinates[this.ta][1],
       textureCoordinates[this.tb][1],
       textureCoordinates[this.tc][1]
     );
-    tDivZ.multiply(zInverted);
-    tDivZ.scale(255); // texture size of 256 pixels in y direction
+    tDivW.multiply(wInverted);
+    tDivW.scale(255); // texture size of 256 pixels in y direction
 
     // x, y, z components of each vertex normal, scaled by the corresponding inverse z value
-    const nxDivZ = new Vector(
+    const nxDivW = new Vector(
       normalCoordinates[this.na][0],
       normalCoordinates[this.nb][0],
       normalCoordinates[this.nc][0]
     );
-    nxDivZ.multiply(zInverted);
+    nxDivW.multiply(wInverted);
 
-    const nyDivZ = new Vector(
+    const nyDivW = new Vector(
       normalCoordinates[this.na][1],
       normalCoordinates[this.nb][1],
       normalCoordinates[this.nc][1]
     );
-    nyDivZ.multiply(zInverted);
+    nyDivW.multiply(wInverted);
 
-    const nzDivZ = new Vector(
+    const nzDivW = new Vector(
       normalCoordinates[this.na][2],
       normalCoordinates[this.nb][2],
       normalCoordinates[this.nc][2]
     );
-    nzDivZ.multiply(zInverted);
+    nzDivW.multiply(wInverted);
 
     let zBufferOffset = ymin * screenBuffer.width + xmin;
     let imageOffset = zBufferOffset * 4;
@@ -187,43 +179,43 @@ export default class Triangle {
     if (nWeight < 0) nWeight = 0;
 
     for (let y = ymin; y < ymax; y++) {
-      this.w.copy(this.wLeft);
-
-      // TODO Calculate pre-step (first triangle x) using deltas (see Delatin)
-      // Also consider breaking x loop (detect we have been inside, but aren't anymore)
-
+      this.k.copy(this.kLeft);
       for (let x = xmin; x < xmax; x++) {
-        if (this.w[0] > 0 && this.w[1] > 0 && this.w[2] > 0) {
-          const zValue = projectedZ.dot(this.w);
-          if (zValue > zBuffer[zBufferOffset]) {
+        if (this.k[0] > 0 && this.k[1] > 0 && this.k[2] > 0) {
+          // the z buffer contains linearly interpolated *projected* z values
+          const perspectiveZ = projectedZ.dot(this.k);
+          if (perspectiveZ > zBuffer[zBufferOffset]) {
             Triangle.pixelsDrawn++;
 
             if (zBuffer[zBufferOffset] > 0) Triangle.pixelsOverdrawn++;
-            zBuffer[zBufferOffset] = zValue;
+            zBuffer[zBufferOffset] = perspectiveZ;
 
             // interpolate the normal vector (based on vertex normals)
             // since we normalize the normal vector in the next step (before using it in the lighting calculation)
             // we can skip the otherwise needed scaling of each vector component by camera space z value
             // thus, only the sign (and not the magnitude) of z matters. the sign will always will be negative
             // since camera is placed in origo and looks down negative z, and the object is placed in negative z space
-            //            this.n[0] = -nxDivZ.dot(this.w);
-            //            this.n[1] = -nyDivZ.dot(this.w);
-            //            this.n[2] = -nzDivZ.dot(this.w);
+            //            this.n[0] = -nxDivW.dot(this.k);
+            //            this.n[1] = -nyDivW.dot(this.k);
+            //            this.n[2] = -nzDivW.dot(this.k);
             //            this.n.normalize();
             // TODO find approximation to normal interpolation and subsequent normalizing
 
             //            nWeight = -this.n.dot(this.lightDirection);
             //            if (nWeight < 0) nWeight = 0;
 
-            // get interpolated 1/z value (based on each of the vertex 1/z values) for this point in the triangle
-            const interpolatedZInverted = zInverted.dot(this.w);
-            // get perspective corrected z value - to use in uv interpolation
-            const perspectiveZ = 1 / interpolatedZInverted;
+            // while, vertex attributes are calculated by interpolating 1/w
 
-            // calculate s/z and t/z values for this point in the triangle, using linear interpolation
-            // then multiply by perspective correct z to get actual s and t values
-            const s = sDivZ.dot(this.w) * perspectiveZ;
-            const t = tDivZ.dot(this.w) * perspectiveZ;
+            // get interpolated 1/w value (based on each of the vertex 1/w values) for this point in the triangle
+            const interpolatedWInverted = wInverted.dot(this.k);
+            // get actual w value - to use in uv interpolation
+            const interpolatedW = 1 / interpolatedWInverted;
+
+            // calculate s/w and t/w values for this point inside the triangle, by
+            // linearly interpolating the corner attribute values (using barycentric weights)
+            // and then multiplying by the interpolated w - to get to actual s and t values
+            const s = sDivW.dot(this.k) * interpolatedW;
+            const t = tDivW.dot(this.k) * interpolatedW;
 
             const tx = Math.trunc(s);
             const ty = Math.trunc(t);
@@ -237,11 +229,11 @@ export default class Triangle {
         }
         imageOffset += 4;
         zBufferOffset++;
-        this.w.sub(dwdx);
+        this.k.sub(dkdx);
       }
       imageOffset += imageStride;
       zBufferOffset += stride;
-      this.wLeft.add(dwdy);
+      this.kLeft.add(dkdy);
     }
   }
 }
